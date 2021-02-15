54e1b1c043038e3efc1c9cf59e14a042708fe192
gh05t-pl <gozetero@gmail.com>
2021-02-15 19:11:21 +0100

    added cors bundle

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
 ];
