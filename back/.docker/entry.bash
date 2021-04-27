92e7236f801a58d6a7783870fb21a6661531dc48
gh05t-pl <w.zmudzinski01@gmail.com>
2021-04-27 15:39:02 +0200

    fixed cron

--- back/.docker/entry.bash
+++ back/.docker/entry.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-cat /etc/cron.d/*.cron | crontab -
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+PATH_TO_CRONS="/.docker/crons/*.cron"
+printenv | cat - $SCRIPTPATH$PATH_TO_CRONS | crontab -
 cron -f &
 docker-php-entrypoint php-fpm
